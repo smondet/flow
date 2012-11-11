cf0a87270484879960895d2b61aefcd3e3a74f81
Sebastien Mondet <seb@mondet.org>
2012-11-11 17:17:01 -0500

    Net: add module for TCP/TLS communications

---
+++ src/lib/flow_net.ml
@@ -0,0 +1,257 @@
+open Core.Std
+open Flow_base
+open Flow_list
+open Flow_sys
+
+let dbg fmt =
+  ksprintf (fun s ->
+    let indented =
+      s |! String.split ~on:'\n' |! String.concat ~sep:"\n          " in
+    wrap_io (Lwt_io.eprintf "DEBUG:   %s\n%!") indented) fmt
+  
+module Tls = struct
+
+  let accept socket context =
+    bind_on_error
+      (catch_io (Lwt_ssl.ssl_accept socket) context)
+      (fun e -> error (`tls_accept_error e))
+
+  let server_socket ~port =
+    let open Lwt_unix in
+    try
+    let fd = socket PF_INET SOCK_STREAM 6 in
+    bind fd (ADDR_INET (Unix.Inet_addr.bind_any, port));
+    listen fd port;
+    return fd
+    with
+    | e -> error (`socket_creation_exn e)
+
+  let tls_connect socket ssl_context =
+    wrap_io (Lwt_ssl.ssl_connect socket) ssl_context
+
+  let tls_shutdown socket =
+    wrap_io Lwt_ssl.ssl_shutdown socket >>= fun () ->
+    wrap_io (fun  () ->
+      Lwt_ssl.shutdown socket Lwt_unix.SHUTDOWN_ALL;
+      Lwt.return ()) () >>= fun () ->
+    wrap_io Lwt_ssl.close socket
+
+  module M_ugly_ssl_get_certificate = struct
+    type ttt = Plain | SSL of Ssl.socket
+    type lwt_ssl_socket = Lwt_unix.file_descr * ttt
+
+    let get_certificate sslsock =
+      begin match snd (Obj.magic sslsock : lwt_ssl_socket) with
+      | Plain -> error (`not_an_ssl_socket)
+      | SSL s ->
+        Lwt.(
+          catch
+            (fun () ->
+              Lwt_preemptive.detach Ssl.get_certificate s >>= fun cert ->
+              return (Ok cert))
+            (function
+            | Ssl.Certificate_error ->
+              return (Error `ssl_certificate_error)
+            | e -> 
+              return (Error (`io_exn e))))
+      end
+  end
+  let tls_get_certificate = M_ugly_ssl_get_certificate.get_certificate
+
+
+  let server_context ?ca_certificate (cert_file, key_file) =
+    let open Ssl in
+    try
+      let c = create_context TLSv1 Server_context in
+      use_certificate c cert_file key_file;
+      set_cipher_list c "TLSv1";
+      Option.iter ca_certificate (fun ca_cert ->
+        set_verify c [ Verify_peer; ] None;
+        set_verify_depth c 99;
+        load_verify_locations c ca_cert "";
+      );
+      return c
+    with e -> error (`tls_context_exn e)
+
+
+  let accept_loop
+      ?(on_error: 'a -> (unit, [> ]) Flow_base.t =fun e -> return ())
+      ?check_client_certificate ?tls_context ~port f =
+    server_socket ~port >>= fun socket ->
+    let handle_one accepted =
+      begin match tls_context with
+      | Some ssl_context ->
+        accept (fst accepted) ssl_context
+      | None -> return (Lwt_ssl.plain (fst accepted))
+      end
+      >>= fun ssl_accepted ->
+      begin match check_client_certificate with
+      | Some ccc ->
+        double_bind (tls_get_certificate ssl_accepted)
+          ~error:(function
+          | `ssl_certificate_error ->
+            return (`invalid_client `wrong_certificate)
+          | `not_an_ssl_socket | `io_exn _ as e -> error e)
+          ~ok:(fun cert ->
+            ccc cert >>= function
+            | `valid name -> return (`valid_client (name: string))
+            | `expired (name, time) ->
+              return (`invalid_client (`expired ((name: string), (time: Time.t))))
+            | `revoked (name, time) ->
+              return (`invalid_client (`revoked ((name: string), (time: Time.t))))
+            | `not_found name ->
+              return (`invalid_client (`not_found (name: string))))
+      | None -> return `anonymous_client
+      end
+      >>= fun client ->
+      f ssl_accepted client
+    in
+    let rec accept_loop c =
+      wrap_io (Lwt_unix.accept_n socket) 10
+      >>= fun (accepted_list, potential_exn) ->
+      map_option potential_exn (fun exn -> on_error (`accept_exn exn))
+      >>= fun (_ : unit option) ->
+      (* dbg "Accepted %d connections (unix)%s" (List.length accepted_list) *)
+      (*   (Option.value_map ~default:"" potential_exn *)
+      (*      ~f:(fun e -> sprintf ", Exn: %s" (Exn.to_string e))) *)
+      (* >>= fun () -> *)
+      accept_loop (c + 1) |! Lwt.ignore_result;
+      Lwt.(
+        Lwt_list.map_p handle_one accepted_list
+        >>= fun res_l ->
+        Lwt_list.map_p (function
+        | Ok () -> return (Ok ())
+        | Error e -> on_error e) res_l
+        >>= fun _ ->
+        return (Ok ()))
+    in
+    accept_loop 0 |! Lwt.ignore_result;
+    return ()
+
+  let client_context ?verification_policy kind =
+    let open Ssl in
+    begin
+      try
+        let c = create_context TLSv1 Client_context in
+        begin match kind with
+        | `anonymous -> ()
+        | `with_certificate (cert, key) ->
+          use_certificate c cert key
+        end;
+        set_cipher_list c "TLSv1";
+        Option.iter verification_policy (function
+        | `verify_server -> 
+          Ssl.set_verify_depth c 99;
+          set_verify c [Verify_peer] (Some client_verify_callback);
+        | `allow_self_signed -> ()
+        );
+        return c
+      with e -> error (`tls_context_exn e)
+    end
+
+
+end
+
+let init_tls = Ssl.init ~thread_safe:true
+    
+type connection = {
+  inchan: Lwt_io.input_channel;
+  outchan: Lwt_io.output_channel;
+  tls_socket: Lwt_ssl.socket;
+}
+let connection inchan outchan tls_socket =
+  {inchan; outchan; tls_socket}
+
+let in_channel t = t.inchan
+let out_channel t = t.outchan
+let shutdown {tls_socket; inchan; outchan} =
+  Tls.tls_shutdown tls_socket >>= fun () ->
+  wrap_io Lwt_io.close inchan >>= fun () ->
+  wrap_io Lwt_io.close outchan >>= fun () ->
+  return ()
+
+type client_check_result =
+[ `expired of string * Core.Std.Time.t
+| `not_found of string
+| `revoked of string * Core.Std.Time.t
+| `valid of string ]
+
+type client_kind = 
+[ `anonymous_client
+| `invalid_client of
+    [ `expired of string * Core.Std.Time.t
+    | `not_found of string
+    | `revoked of string * Core.Std.Time.t
+    | `wrong_certificate ]
+| `valid_client of string ]
+
+let plain_server ?on_error ~port f =
+  Tls.accept_loop ~port ?on_error
+    (fun socket_fd _ ->
+      let inchan = Lwt_ssl.in_channel_of_descr  socket_fd in
+      let outchan = Lwt_ssl.out_channel_of_descr socket_fd in
+      f (connection inchan outchan socket_fd))
+
+let tls_server ?on_error ~port ~cert_key f =
+  Tls.server_context cert_key
+  >>= fun tls_context ->
+  Tls.accept_loop ~tls_context ?on_error ~port
+    (fun socket_fd client_kind ->
+      let inchan = Lwt_ssl.in_channel_of_descr  socket_fd in
+      let outchan = Lwt_ssl.out_channel_of_descr socket_fd in
+      f (connection inchan outchan socket_fd))
+  
+  
+let authenticating_tls_server
+    ~ca_certificate ~check_client_certificate
+    ?on_error ~port ~cert_key f = 
+  Tls.server_context ~ca_certificate cert_key
+  >>= fun tls_context ->
+  Tls.accept_loop ?on_error ~check_client_certificate ~tls_context ~port
+    (fun socket_fd client_kind ->
+      let inchan = Lwt_ssl.in_channel_of_descr  socket_fd in
+      let outchan = Lwt_ssl.out_channel_of_descr socket_fd in
+      f (connection inchan outchan socket_fd) client_kind)
+
+
+type connection_specification = [
+| `tls of
+    [ `anonymous | `with_certificate of string * string ]
+  * [ `verify_server | `allow_self_signed ]
+| `plain
+]
+  
+let unix_connect sockaddr =
+  let socket =
+    Lwt_unix.(
+      try
+        let fd = socket PF_INET SOCK_STREAM 0 in
+        fd
+      with
+      | Unix.Unix_error (e, s, a) as ex ->
+        eprintf "Unix.Unix_error: %s %s %s\n%!" (Unix.error_message e) s a;
+        raise ex
+    ) in
+  wrap_io (Lwt_unix.connect socket) sockaddr
+  >>= fun () ->
+  return socket
+    
+let connect ~address specification =
+  begin match specification with
+  | `plain ->
+    unix_connect address >>= fun unix_socket_fd ->
+    let socket_fd = Lwt_ssl.plain unix_socket_fd in
+    let inchan = Lwt_ssl.in_channel_of_descr  socket_fd in
+    let outchan = Lwt_ssl.out_channel_of_descr socket_fd in
+    return (connection inchan outchan socket_fd)
+  | `tls (connection_type, verification_policy) ->
+    unix_connect address >>= fun unix_socket_fd ->
+    Tls.client_context ~verification_policy connection_type >>= fun tls_context ->
+    Tls.tls_connect unix_socket_fd tls_context >>= fun socket_fd ->
+    let inchan = Lwt_ssl.in_channel_of_descr  socket_fd in
+    let outchan = Lwt_ssl.out_channel_of_descr socket_fd in
+    return (connection inchan outchan socket_fd)
+  end
+
+
+    
---
+++ src/lib/flow_net.mli
@@ -0,0 +1,123 @@
+(** High-level TCP + TLS connection handling. *)
+
+
+(** {3 TLS Initialization} *)
+
+val init_tls :  unit -> unit
+(** Initialize the SSL library. *)
+
+(** {3 Generic Connection Handle} *)
+
+(** A connection is full duplex and can be shut down. *)
+type connection
+val in_channel: connection -> Lwt_io.input_channel 
+val out_channel: connection -> Lwt_io.output_channel 
+
+(**  Note that [shutdown connection] closes the channels. *) 
+val shutdown : connection -> (unit, [> `io_exn of exn ]) Flow_base.t
+
+(** {3 Client Connection} *)
+
+type connection_specification = [
+| `tls of
+    [ `anonymous | `with_certificate of string * string ]
+  * [ `verify_server | `allow_self_signed ]
+| `plain
+]
+(** Specification of the kind of connection (for the function [connect]). *)
+    
+val connect: address:Lwt_unix.sockaddr -> connection_specification ->
+  (connection, [> `io_exn of exn | `tls_context_exn of exn ]) Flow_base.t
+(** Connect to the server at [address]. *)
+
+(** {3 Server Establishment} *)
+
+val plain_server :
+  ?on_error:(([> `accept_exn of Core.Std.Exn.t
+              | `io_exn of exn
+              | `not_an_ssl_socket
+              | `tls_accept_error of exn ]
+                 as 'errors) ->
+             (unit, [> `io_exn of exn ]) Flow_base.t) ->
+  port:int ->
+  (connection -> (unit, 'errors) Flow_base.t) ->
+  (unit, [> `io_exn of exn | `socket_creation_exn of exn ]) Flow_base.t
+(** Start a “plain” TCP server on port [port]. This function returns
+    immediately, the “accept-loop” runs in {i Lwt} threads.
+
+    The function [on_error] is an error handler, it will be called on
+    "acceptation" errors (c.f.
+    {{:http://ocsigen.org/lwt/api/Lwt_unix#VALaccept_n}Lwt_unix.accept_n})
+    {b and} on the remaining errors of the handler (but errors of the
+    [on_error] function itself will be ignored).
+
+    Example: {[
+    plain_server ~port:4242
+      ~on_error:(fun e -> logf "Error: %s" (string_of_error e))
+      (fun connection ->
+         Flow_base_io.bin_send connection#out_channel "Hello !!"
+         >>= fun () ->
+         connection#shutdown)
+    ]}
+*)
+
+val tls_server :
+  ?on_error:(([> `accept_exn of Core.Std.Exn.t
+              | `io_exn of exn
+              | `not_an_ssl_socket
+              | `tls_accept_error of exn ]
+                 as 'a) ->
+             (unit, [> `io_exn of exn ]) Flow_base.t) ->
+  port:int ->
+  cert_key:string * string ->
+  (connection -> (unit, 'a) Flow_base.t) ->
+  (unit, [> `socket_creation_exn of exn | `tls_context_exn of exn ])
+           Flow_base.t
+(** Like [plain_server] but with a TLS layer, the server will be
+    authenticated with [~cert_key:("cert.crt", "k.key")]. *)
+
+type client_check_result =
+[ `expired of string * Core.Std.Time.t
+| `not_found of string
+| `revoked of string * Core.Std.Time.t
+| `valid of string ]
+(** The result type expected from [check_client_certificate] functions. *)
+
+type client_kind =
+[ `anonymous_client
+| `invalid_client of
+    [ `expired of string * Core.Std.Time.t
+    | `not_found of string
+    | `revoked of string * Core.Std.Time.t
+    | `wrong_certificate ]
+| `valid_client of string ]
+(** The different kinds of clients that a authenticating TLS server
+    handler has to treat separately.
+
+    In the current implementation, the case [`anonymous_client] is
+    never used, clients without certificate are seen as
+    [`invalid_client `wrong_certificate].
+*)
+
+val authenticating_tls_server :
+  ca_certificate:string ->
+  check_client_certificate:(Ssl.certificate ->
+                            (client_check_result,
+                             [> `accept_exn of Core.Std.Exn.t
+                             | `io_exn of exn
+                             | `not_an_ssl_socket
+                             | `tls_accept_error of exn ]
+                               as 'a)
+                              Flow_base.t) ->
+  ?on_error:('a -> (unit, [> `io_exn of exn ]) Flow_base.t) ->
+  port:int ->
+  cert_key:string * string ->
+  (connection -> client_kind ->
+   (unit, 'a) Flow_base.t) ->
+  (unit, [> `socket_creation_exn of exn | `tls_context_exn of exn ])
+    Flow_base.t
+(** Start an authenticating TLS server,  the [ca_certificate] is used to check
+    client certificates, and the function [check_client_certificate] to
+    decide about the CA-validity of the certificate once it has been validated
+    with respect to the TLS protocol. *)
+
