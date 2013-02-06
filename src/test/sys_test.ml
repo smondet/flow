bd4333473aae4e1f27616435aabbd57ae60ad349
Sebastien Mondet <seb@mondet.org>
2013-02-06 16:36:22 -0500

    sys_test: test function System.copy

--- src/test/sys_test.ml
+++ src/test/sys_test.ml
@@ -68,20 +68,52 @@ let test_file_info () =
   say "test_file_info: OK";
   return ()
 
+let is_present ?(and_matches=(fun _ -> true)) path =
+  System.file_info path
+  >>= begin function
+  | `absent -> error (`wrong_file_info (path, `absent))
+  | any when and_matches any -> return ()
+  | any_other -> error (`wrong_file_info (path, any_other))
+  end
+
+let is_absent path =
+  System.file_info path
+  >>= begin function
+  | `absent -> return ()
+  | e -> error (`wrong_file_info (path, e))
+  end
+
+let random_tree path max_number_creations =
+  let max_number_creations = ref max_number_creations in
+  let rec random_tree_aux path =
+    while_sequential (List.init (Random.int 20 + 20) ident) (fun id ->
+    if !max_number_creations <= 0 then return ()
+    else begin
+      let path n =
+        ksprintf (Filename.concat path) "random_%s_%d" n id in
+      decr max_number_creations;
+      begin match Random.int 3 with
+      | 0 ->
+        let p = path "dir" in
+        System.mkdir_p p
+        >>= fun () ->
+        random_tree_aux p
+      | 1 ->
+        let size = Random.int 1_000_000 in
+        let content = String.create size in
+        let p = ksprintf path "file_of_size_%d" size in
+        IO.write_file p ~content
+      | _ ->
+        let p = path "symlink" in
+        System.make_symlink ~target:"/tmp/bouh" ~link_path:p
+      end
+    end)
+    >>= fun _ ->
+    return ()
+  in
+  random_tree_aux path
+
 let test_remove style =
-  let is_present ?(and_matches=(fun _ -> true)) path =
-    System.file_info path
-    >>= begin function
-    | `absent -> error (`wrong_file_info (path, `absent))
-    | any when and_matches any -> return ()
-    | any_other -> error (`wrong_file_info (path, any_other))
-    end in
-  let is_absent path =
-    System.file_info path
-    >>= begin function
-    | `absent -> return ()
-    | e -> error (`wrong_file_info (path, e))
-    end in
   let in_dir =
     match style with
     | `relative -> "test_flow_system_remove"
@@ -112,26 +144,7 @@ let test_remove style =
   say "Removed %s" test_empty_dir;
   let test_non_empty_dir = Filename.concat in_dir "non_empty_dir" in
   System.mkdir_p test_non_empty_dir >>= fun () ->
-  let max_number_creations = ref 100 in
-  let rec random_tree path =
-    while_sequential (List.init (Random.int 20) ident) (fun n ->
-    if !max_number_creations <= 0 then return ()
-    else begin
-      let name =
-        ksprintf (Filename.concat path) "random%d" n in
-      decr max_number_creations;
-      if Random.bool ()
-      then begin
-        System.mkdir_p name >>= fun () ->
-        random_tree name
-      end else begin
-        IO.write_file name ~content:"lkjsdflkjs"
-      end
-    end)
-    >>= fun _ ->
-    return ()
-  in
-  random_tree test_non_empty_dir
+  random_tree test_non_empty_dir 100
   >>= fun () ->
   cmdf "find %s | wc -l" test_non_empty_dir
   >>= fun () ->
@@ -152,15 +165,106 @@ let test_remove style =
   say "test_remove: OK";
   return ()
 
+let fail_test fmt =
+  ksprintf (fun s -> error (`failed_test s)) fmt
+
+let test_copy style_in style_out =
+  let out_dir =
+    match style_out with
+    | `relative -> "test_flow_system_copy_target"
+    | `absolute -> "/tmp/test_flow_system_copy_target" in
+  let in_dir =
+    match style_in with
+    | `relative -> "test_flow_system_copy"
+    | `absolute -> "/tmp/test_flow_system_copy" in
+  say "test_copy %s/… → %s/…" in_dir out_dir;
+  cmdf "rm -fr %s" in_dir >>= fun () ->
+  cmdf "rm -fr %s" out_dir >>= fun () ->
+  System.mkdir_p in_dir >>= fun () ->
+  System.mkdir_p out_dir >>= fun () ->
+
+  let test_symlink = Filename.concat in_dir "test_symlink" in
+  System.make_symlink ~target:"/tmp/bouh" ~link_path:test_symlink
+  >>= fun () ->
+
+  say "`redo symlinks %s into %s" test_symlink out_dir;
+  System.copy ~symlinks:`redo ~src:test_symlink (`into_directory out_dir)
+  >>= fun () ->
+  let expected_path = Filename.concat out_dir "test_symlink" in
+  is_present ~and_matches:((=) (`symlink "/tmp/bouh")) expected_path
+  >>= fun () ->
+
+  say "redo symlink %s as %s" test_symlink out_dir;
+  let dst = Filename.concat out_dir "test_symlink_new" in
+  System.copy ~symlinks:`redo ~src:test_symlink (`as_new dst)
+  >>= fun () ->
+  is_present ~and_matches:((=) (`symlink "/tmp/bouh")) dst
+  >>= fun () ->
+  System.remove dst (* we remove this one to be able to call find at the end *)
+  >>= fun () ->
+
+  let test_copy_file size =
+    let test_reg_file = Filename.concat in_dir (sprintf "reg_file_%d" size) in
+    let content = String.make size 'B' in
+    IO.write_file test_reg_file ~content
+    >>= fun () ->
+    System.copy ~src:test_reg_file (`into_directory out_dir)
+    >>= fun () ->
+    let expected_path = Filename.(concat out_dir (basename test_reg_file)) in
+    is_present ~and_matches:((=) (`file size)) expected_path
+    >>= fun () ->
+    IO.read_file expected_path
+    >>= fun content_got ->
+    if content_got = content then return ()
+    else
+      fail_test "contents of %s and %s are different" test_reg_file expected_path
+  in
+  test_copy_file 200 >>= fun () ->
+  test_copy_file 200_000 >>= fun () ->
+
+  let subtree_path = Filename.concat in_dir "random_tree" in
+  System.mkdir_p subtree_path
+  >>= fun () ->
+  random_tree subtree_path 20
+  >>= fun () ->
+
+  System.copy ~symlinks:`redo ~src:subtree_path (`into_directory out_dir)
+  >>= fun () ->
+
+  ksprintf System.get_system_command_output "find %s | wc -l" in_dir
+  >>= fun (find_in, _) ->
+  ksprintf System.get_system_command_output "find %s | wc -l" out_dir
+  >>= fun (find_out, _) ->
+
+  if find_in <> find_out then (
+    say "find_in: %s" find_in;
+    say "find_out: %s" find_out;
+    fail_test "find_in <> find_out"
+  ) else (
+    return ()
+  )
+     >>= fun () ->
+
+  say "test_copy: OK";
+  return ()
+
 
 let main () =
   say "sys_test: GO!";
+  let (>>=) m f =
+    m >>= (fun o ->
+      say "=======================================";
+      f o) in
   test_mkdir ()
   >>= fun () ->
   test_file_info ()
   >>= fun () ->
   test_remove `relative >>= fun () ->
   test_remove `absolute >>= fun () ->
+  test_copy `relative `relative  >>= fun () ->
+  test_copy `absolute `absolute  >>= fun () ->
+  test_copy `absolute `relative  >>= fun () ->
+  test_copy `relative `absolute  >>= fun () ->
   say "sys_test: Successful End";
   return ()
 
@@ -172,11 +276,18 @@ let () =
       (<:sexp_of<
           [ `system of
               [`file_info of string | `mkdir of string
+              | `copy of string
               | `make_symlink of string * string
               | `remove of string | `list_directory of string ] *
-                [ `already_exists | `exn of exn | `wrong_access_rights of int ]
+                [ `already_exists
+                | `file_not_found of string
+                | `wrong_file_kind of string *
+                    [ `block_device | `character_device | `fifo | `socket | `symlink of string ]
+                | `exn of exn | `wrong_access_rights of int ]
+          | `failed_test of string
           | `io_exn of exn
           | `write_file_error of string * exn
+          | `read_file_error of string * exn
           | `wrong_file_info of string *
               [ `absent
               | `block_device
